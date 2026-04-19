28c54b4a549aca5357059c2a22e1536c0e0f987c
Matthew Martin <mmartin10@arizona.edu>
2026-04-19 18:56:24 +0000

    Update in SQL code to try to fix an error

--- sql/california/query2_protected_area_by_county.sql
+++ sql/california/query2_protected_area_by_county.sql
@@ -1,12 +1,12 @@
 
--- Query 2: Proteted  Area by County
+-- Query 2: Protected  Area by County
 -- Purpose: Calculate the total area of protected areas in each county
 
 -- Requirements:
 -- - Use adminareas_a for counties (fclass = 'admin_level6')
--- - Use landuse_a for parks (fclass = 'park')
--- - Use ST_Intersects to join parks to counties
--- - Use ST_Intersection to clip park geometries to county boundaries
+-- - Use protected_areas_a for protected areas (fclass = 'national_park')
+-- - Use ST_Intersects to join protected areas to counties
+-- - Use ST_Intersection to clip protected area geometries to county boundaries
 -- - Use ST_Area(geom::geography) for accurate measurements on WGS84
 -- - Convert square meters to square kilometers (divide by 1,000,000)
 -- - Group results by county name
@@ -19,15 +19,15 @@
 
 SELECT
     aa.name AS county_name,
-    SUM(ST_Area(ST_Intersection(p.geom, aa.geom)::geography)) / 1000000 AS protected_area_sq_km,
+    SUM(ST_Area(ST_Intersection(pa.geom, aa.geom)::geography)) / 1000000 AS protected_area_sq_km,
     aa.geom
 FROM
     adminareas_a AS aa
 JOIN
-    protected_areas_a AS p ON ST_Intersects(aa.geom, p.geom)
+    protected_areas_a AS pa ON ST_Intersects(aa.geom, pa.geom)
 WHERE
     aa.fclass = 'admin_level6'
-    AND p.fclass = 'national_park'
+    AND pa.fclass = 'national_park'
 GROUP BY
     aa.name, aa.geom
 ORDER BY
